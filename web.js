ea1b4b9025dc19733affefac3f373945e605d249
Adolfo <amartinez@intelimetrica.com>
2013-09-10 22:15:06 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello again World!');
+  response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
